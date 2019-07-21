3b2096945797f355e977882813067cc23d881f96
andreb123 <andrescottbush@hotmail.com>
2019-07-21 17:13:28 -0500

    pucshing broken code to test

---
+++ src/app/service/jwtTokenCeck.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// intercepts http requests and adds authorization header with jwt token if available
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // checks the storage for the current user
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        //adds authorization the current request header 
+        if (currentUser && currentUser.token) {
+            request = request.clone({
+                setHeaders: { 
+                    Authorization: `Bearer ${currentUser.token}`
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
