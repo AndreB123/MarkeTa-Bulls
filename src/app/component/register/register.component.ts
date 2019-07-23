ce802eb9cff5300a4693901fb82aea4d05286a0c
andreb123 <andrescottbush@hotmail.com>
2019-07-23 13:08:41 -0500

    jimmy broke it

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -24,8 +24,6 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
       this.registerForm = this.formBuilder.group({
-          firstName: ['', Validators.required],
-          lastName: ['', Validators.required],
           username: ['', Validators.required],
           password: ['', [Validators.required, Validators.minLength(6)]]
       });
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,10 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { headersToString } from 'selenium-webdriver/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../classes/user';
 
 @Injectable()
 export class AuthService {
-    constructor(private http: HttpClient) { }
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+     }
+
+     public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
 
   login(username: string, password: string) {
       console.log(username);
@@ -15,10 +27,11 @@ export class AuthService {
           .pipe(map(user => {
               // login successful if there's a jwt token in the response
               console.log(user);
-              if (user && user.token) {
+              if (user) {
                   // store user details and jwt token in local storage to keep user logged in between page 'refreshes'
                   console.log(user);
                   localStorage.setItem('currentUser', JSON.stringify(user));
+                  this.currentUserSubject.next(user);
               }
 
                 return user;
@@ -28,6 +41,6 @@ export class AuthService {
     logout() {
         // remove user from local storage and ends session to log user out
         localStorage.removeItem('currentUser');
-        this.http.post<any>(`http://localhost:8080/pipelineTest/MarkeTa-Bulls/logout`, {});
+        this.currentUserSubject.next(null);
     }
 }
