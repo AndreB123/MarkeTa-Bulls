3fad7600b83b0d71bde8da3b43935f6095a5eb09
jhagle4 <haglejimmie@gmail.com>
2019-07-23 11:04:50 -0700

    Login Works!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
           .pipe(map(user => {
               // login successful if there's a jwt token in the response
               console.log(user);
-              if (user && user.token) {
+              if (user) {
                   // store user details and jwt token in local storage to keep user logged in between page 'refreshes'
                   console.log(user);
                   localStorage.setItem('currentUser', JSON.stringify(user));
