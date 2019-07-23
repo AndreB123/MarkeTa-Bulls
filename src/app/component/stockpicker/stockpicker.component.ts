11a9eec67da5b406498888782460d40870b903c2
jhagle4 <haglejimmie@gmail.com>
2019-07-22 18:48:39 -0700

    stock adding

--- src/app/component/stockpicker/stockpicker.component.ts
+++ src/app/component/stockpicker/stockpicker.component.ts
@@ -14,7 +14,7 @@ export class StockpickerComponent implements OnInit {
   private stocks:any[];
 
   ngOnInit() {
-    this.displayMarkets();
+    //this.displayMarkets();
     //this.displayStocks();
     let btn = document.getElementById("SButton");
     btn.addEventListener("click", (e:Event) => this.displayStocks());
@@ -75,20 +75,78 @@ export class StockpickerComponent implements OnInit {
         this.stocks = data["quoteResponse"]["result"];
         var page = document.getElementById("stocks");
         console.log(this.stocks);
-        page.innerHTML = "";
+        var count = 0;
         for(let o of this.stocks){
-          page.innerHTML += o["fullExchangeName"] + "<br>";
-          page.innerHTML += o["symbol"]+ "<br>";
-          page.innerHTML += o["longName"]+ "<br>";
-          page.innerHTML +="Short Term Trend: " + o["pageViews"]["shortTermTrend"];
-          page.innerHTML += " | Mid Term Trend: " + o["pageViews"]["midTermTrend"];
-          page.innerHTML += " | Long Term Trend: " + o["pageViews"]["longTermTrend"] + "<br>";
-          page.innerHTML += "Pre Market Change: " + o["preMarketChangePercent"] + "%<br>";
-          page.innerHTML += "Pre Market Price: $" + o["preMarketPrice"] + "<br>";
-          page.innerHTML += "Previous Close Market Price: $" + o["regularMarketPreviousClose"] + "<br>";
-          page.innerHTML += "Market Price: $" + o["regularMarketPrice"] + "<br>";
-          page.innerHTML += "Percent Change: " + o["regularMarketChangePercent"] + "%<br>";
-          page.innerHTML += "Market Volume: " + o["regularMarketVolume"] + "<br>";
+          var results = document.createElement("form");
+          results.setAttribute("id", "result" + count);
+          results.setAttribute("method", "post");
+          results.setAttribute("action", "http://localhost:8080/pipelineTest/MarkeTa-Bulls/stock/add");
+
+          var exchangename = document.createElement("label");
+          exchangename.setAttribute("id", "ename"+count);
+          exchangename.innerHTML += o["fullExchangeName"];
+          results.appendChild(exchangename);
+
+          var symbol = document.createElement("label");
+          symbol.setAttribute("id", "symbol"+count);
+          symbol.innerHTML += o["symbol"];
+          results.appendChild(symbol);
+          var hSymbol = document.createElement("input");
+          hSymbol.setAttribute("name", "symbol");
+          hSymbol.setAttribute("type", "hidden");
+          hSymbol.value = o["symbol"];
+
+          var fullname = document.createElement("label");
+          fullname.setAttribute("id", "fname"+count);
+          fullname.innerHTML += o["longName"];
+          results.appendChild(fullname);
+
+          var trends = document.createElement("label");
+          trends.setAttribute("id", "trends"+count);
+          trends.innerHTML +="Short Term Trend: " + o["pageViews"]["shortTermTrend"];
+          trends.innerHTML += " | Mid Term Trend: " + o["pageViews"]["midTermTrend"];
+          trends.innerHTML += " | Long Term Trend: " + o["pageViews"]["longTermTrend"];
+          results.appendChild(trends);
+
+          var preMarketChange = document.createElement("label");
+          preMarketChange.setAttribute("id", "preMarketChange"+count);
+          preMarketChange.innerHTML += "Pre Market Change: " + o["preMarketChangePercent"] + "%";
+          results.appendChild(preMarketChange);
+
+          var preMarketPrice = document.createElement("label");
+          preMarketPrice.setAttribute("id", "preMarketPrice"+count);
+          preMarketPrice.innerHTML += "Pre Market Price: $" + o["preMarketPrice"];
+          results.appendChild(preMarketPrice);
+
+          var prevMarketClose = document.createElement("label");
+          prevMarketClose.setAttribute("id", "preMarketClose"+count);
+          prevMarketClose.innerHTML += "Previous Close Market Price: $" + o["regularMarketPreviousClose"];
+          results.appendChild(prevMarketClose);
+          
+          var marketPrice = document.createElement("label");
+          marketPrice.setAttribute("id", "marketPrice"+count);
+          marketPrice.innerHTML += "Market Price: $" + o["regularMarketPrice"];
+          results.appendChild(marketPrice);
+          var hPrice = document.createElement("input");
+          hPrice.setAttribute("name", "price");
+          hPrice.setAttribute("type", "hidden");
+          hPrice.value = o["price"];
+
+          var percentChange = document.createElement("label");
+          percentChange.setAttribute("id", "percentChange"+count);
+          percentChange.innerHTML += "Percent Change: " + o["regularMarketChangePercent"] + "%";
+          results.appendChild(percentChange);
+
+          var volume = document.createElement("label");
+          volume.setAttribute("id", "fname"+count);
+          volume.innerHTML += "Market Volume: " + o["regularMarketVolume"];
+          results.appendChild(volume);
+
+          var add = document.createElement("input");
+          add.setAttribute("type", "submit");
+          add.value = "Buy Stock";
+          results.appendChild(add);
+          page.appendChild(results);
         }
       },
       error => {
